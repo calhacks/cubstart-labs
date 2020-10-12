99cb7094e28b9cfd0c4cdee8d73412f59b179d86
Terrance Li <tyli@berkeley.edu>
2020-10-11 21:03:47 -0700

    Added week 5 ios

---
+++ week-5/ios/Cubstart-Shopping-List/ItemViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ItemViewController.swift
+//  Cubstart-Shopping-List
+//
+//  Created by Alex Lu on 10/11/20.
+//  Copyright © 2020 Alexandra Lu. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+    
+    // IBOutlets have been connected for you :)
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemCount: UILabel!
+
+    // What variables do you need?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Set your outlets
+    }
+
+}
---
+++ week-5/ios/Cubstart-Shopping-List/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Cubstart-Shopping-List
+//
+//  Created by Alex Lu on 10/11/20.
+//  Copyright © 2020 Alexandra Lu. All rights reserved.
+//
+
+import UIKit
+
+// Create your CustomTableView class and create necessary outlets
+
+// Make sure to connect DataSource and Delegate
+class ViewController : UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Arrays and Variables
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Register your table view
+    }
+    
+    // UITableViewDataSource functions
+    
+    // UITableViewDelegate function
+    
+    // Preparing your segue
+}
+
